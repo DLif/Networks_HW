39943d0895734593f8bb0100c269ebabdd7e26cc
MauriceTheObese <denchikg@gmail.com>
2014-12-23 06:14:13 +0200

    header fix

--- client_list.h
+++ client_list.h
@@ -24,4 +24,14 @@ void init_client_list(client_list* list);
 
 void add_client(client_list* list, buffered_socket* node);
 
+
+/*
+
+	method deletes the client from the given client list
+	this method will find the buffered_socket of the given client by the the given id and free it (properly)
+	if no such client exists, the list won't be changed
+*/
+
+void delete_by_client_id(client_list* list, int client_id);
+
 #endif
